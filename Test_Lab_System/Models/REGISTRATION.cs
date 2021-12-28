0fe9dacde445d857ed21cf9bce7fe66ca316a606
1NourHany <88481011+1NourHany@users.noreply.github.com>
2021-12-28 13:23:09 +0200

    Add files via upload

---
+++ Test_Lab_System/Models/REGISTRATION.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Lab_System.Models
+{
+    internal class REGISTRATION
+    {
+        public int PatientID { get; set; }
+        public string PatientFirstName { get; set; }
+        public string PatientLastName { get; set; }
+        public int PatientAge { get; set; }
+        public string PatientGender { get; set; }
+        public int PatientMobilePhone { get; set; }
+        public int PatientNationalID { get; set; }
+        public string PatientAddress { get; set; }
+        public string PatientMaritalStatus { get; set; }
+        public string PatientBloodType { get; set; }
+        public string PatientNationality { get; set; }
+        public string PatientEmail { get; set; }
+        public int PatientDOB { get; set; }
+        public string PatientJob { get; set; }
+        public string PatientMotherName { get; set; }
+        public string InsuranceCompany { get; set; }
+        public string InsuranceAddress { get; set; }
+        public int InsurancePhone { get; set; }
+        public string InsuranceSubscriberName { get; set; }
+        public int PatientHomePhone { get; set; }
+        public string EmergencyContactName { get; set; }
+        public int EmergencyContactMobile { get; set; }
+        public string EmergencyContactRelationshi { get; set; }
+
+    }
+}
